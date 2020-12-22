c4b1a7736511d24921bf64ddcbfc5c6932a062ea
Giulio Martellucci <giulio.martellucci@devo.com>
2020-12-22 16:36:19 +0100

    new attributes in adapter class

--- deeplog_trainer/log_parser/adapter.py
+++ deeplog_trainer/log_parser/adapter.py
@@ -1,11 +1,14 @@
 import re
 
 class BatrasioAdapter:
-    def __init__(self):
+    def __init__(self, delimiter='TCP source connection created', anomaly1='TCP source SSL error',
+                 anomaly2='TCP source socket error'):
         self.d = {}
         self.last_session_id = 0
         self.anomaly_flag = {}
-
+        self.delimiter = delimiter
+        self.anomaly1 = anomaly1
+        self.anomaly2 = anomaly2
     def get_session_id(self, log_msg: str):
         """
         Given the log message, return the corresponding session Id it belongs to.
@@ -15,13 +18,10 @@ class BatrasioAdapter:
         if procid not in self.d.keys():
             self.d[procid] = self.last_session_id + 1
             self.anomaly_flag[self.d[procid]] = False
-        delimiter = 'TCP source connection created'
-        anomaly1 = 'TCP source SSL error'
-        anomaly2 = 'TCP source socket error'
-        if delimiter in log_msg:
+        if self.delimiter in log_msg:
             self.d[procid] = self.last_session_id + 1
             self.last_session_id += 1
             self.anomaly_flag[self.d[procid]] = False
-        if anomaly1 in log_msg or anomaly2 in log_msg:
+        if self.anomaly1 in log_msg or self.anomaly2 in log_msg:
             self.anomaly_flag[self.d[procid]] = True
         return self.d[procid], self.anomaly_flag
