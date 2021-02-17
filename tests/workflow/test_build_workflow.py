559654ef7c50a90ff6c091e4b5d1694b122526ba
Giulio Martellucci <giulio.martellucci@devo.com>
2021-02-17 10:51:48 +0100

    skip duplicate nodes

--- tests/workflow/test_build_workflow.py
+++ tests/workflow/test_build_workflow.py
@@ -1,9 +1,9 @@
 import pytest
 from deeplog_trainer.workflow.build_workflow import WorkflowBuilder
+from deeplog_trainer.workflow.build_network import Node, Network
 import json
 import numpy as np
 import logging as logger
-import pdb
 
 
 def get_dataset(min_length=4):
@@ -21,20 +21,9 @@ def get_dataset(min_length=4):
 
 @pytest.mark.parametrize("dataset, threshold, verbose",
                          [(get_dataset(), 0.2, 1)])
-def test_get_similar_seqs(dataset, threshold, verbose):
-    wb = WorkflowBuilder(logger)
-    max_length = max([len(s) for s in dataset])
-    similar_seqs = wb._get_similar_sequences(dataset, threshold, verbose)
-    assert len(similar_seqs) == len(dataset)
-    assert len(similar_seqs[0][0]) == max_length
-    for seqs in similar_seqs:
-        if len(seqs) == 1:
-            continue
-        original_seqs = seqs
-        seqs = np.random.rand(len(seqs), max_length)
-        for i in range(len(seqs)):
-            seqs[i, :len(original_seqs[i])] = original_seqs[i]
-        seqs_ref = np.repeat([seqs[0]], len(seqs)-1, axis=0)
-        seqs_compare = seqs[1:]
-        scores = np.sum(seqs_ref == seqs_compare, axis=-1)/max_length
-        assert all(scores >= threshold) is True
+def test_build_workflows(dataset, threshold, verbose):
+    workflow_builder = WorkflowBuilder(logger)
+    workflow = workflow_builder.build_workflows(dataset, threshold=threshold,
+                                                verbose=verbose, back_steps=1)
+    assert 6 in workflow['network'].get_node(5).get_children(only_node_idx=True)
+
