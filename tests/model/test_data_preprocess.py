79c4acf2dfaaf83b865393024a92b3d1755525f4
Giulio Martellucci <giulio.martellucci@devo.com>
2021-03-03 18:33:08 +0100

    reorganise some test functions

--- tests/model/test_data_preprocess.py
+++ tests/model/test_data_preprocess.py
@@ -22,29 +22,49 @@ def get_data(min_length=4):
                 # Skip short sequences
                 continue
             dataset.append(seq)
-            yield seq, dataset
+        return dataset
 
-@pytest.mark.parametrize("seq,dataset", get_data())
-def test_preprocess(seq, dataset, setup, window_size=7):
+def get_seqs(min_length=4):
+    filepath = 'data/data.json'
+    with open(filepath, 'r') as fp:
+        data = json.load(fp)
+        for d in data['data']:
+            seq = d['template_seq']
+            if len(seq) < min_length:
+                # Skip short sequences
+                continue
+            yield seq
+
+@pytest.mark.parametrize("dataset, train_ratio, val_ratio",
+                         [(get_data(), 0.7, 0.85)])
+def test_split_idx(dataset, setup, train_ratio, val_ratio):
+    train_idx, val_idx, test_idx = \
+        setup.split_idx(len(dataset), train_ratio, val_ratio)
+    assert (train_idx == range(int(len(dataset) * train_ratio))).all()
+    assert (val_idx == range(int(len(dataset) * train_ratio),
+                             int(len(dataset) * val_ratio))).all()
+    assert (test_idx == range(int(len(dataset) * val_ratio),
+                              len(dataset))).all()
+
+@pytest.mark.parametrize("seq", get_seqs())
+def test_chunks_seq(mocker, seq, window_size=7):
+    mocked_vocab = mocker.patch.object(DataPreprocess, '__init__',
+                                       return_value=None)
+    data_preprocess = DataPreprocess(mocked_vocab)
+    list_of_chunks = data_preprocess.chunks_seq(seq, window_size=window_size)
+    if len(seq) > window_size:
+        assert np.shape(list_of_chunks) == (len(seq) - window_size + 1,
+                                        window_size)
+    else:
+        assert np.shape(list_of_chunks) == (1, len(seq))
+
+@pytest.mark.parametrize("dataset", [(get_data())])
+def test_transform(dataset, setup, window_size=7):
     dataset = np.array(dataset, dtype=object)
     dataset = setup.encode_dataset(dataset)
     assert setup.get_dictionaries()[1]['[PAD]'] == 0
-    list_of_chunks = setup.chunks_seq(seq, window_size=window_size)
-    if len(seq) > window_size:
-        assert np.shape(list_of_chunks) == (len(seq)-window_size+1, window_size)
-    else:
-        assert np.shape(list_of_chunks) == (1, len(seq))
-    train_idx, val_idx, test_idx = \
-        setup.split_idx(len(dataset), train_ratio=0.7, val_ratio=0.85)
-    train_dataset = dataset[train_idx]
-    val_dataset = dataset[val_idx]
-    assert len(train_dataset) == int(len(dataset)*0.7)
-    assert len(train_dataset) + len(val_dataset) == int(len(dataset)*0.85)
-    train_chunks = setup.chunks(train_dataset, window_size=window_size)
-    X_train, y_train = setup.transform(
-        train_chunks,
-        add_padding=window_size
-    )
-    assert np.shape(X_train) == (len(train_chunks), window_size,
-                                 setup.get_num_tokens())
-    assert np.shape(y_train) == (len(train_chunks), setup.get_num_tokens())
+    chunks = setup.chunks(dataset, window_size=window_size)
+    X, y = setup.transform(chunks, add_padding=window_size)
+    assert X.shape == (len(chunks), window_size,
+                       setup.get_num_tokens())
+    assert y.shape == (len(chunks), setup.get_num_tokens())
